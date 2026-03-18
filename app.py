b59665df0e40cce350a7567af812a8e10ba2d837
shuixian71 <shuixian710@gmail.com>
2026-03-18 21:51:41 +0800

    Update the button and page for skin diseasesd modules

--- app.py
+++ app.py
@@ -18,5 +18,25 @@ def information():
 def detection():
     return render_template('detection.html', active_page="detection")
 
+@app.route("/ringworm")
+def ringworm():
+    return render_template('ringworm.html', active_page="information")
+
+@app.route("/hypersensitivity")
+def hypersensitivity():
+    return render_template('hypersensitivity.html', active_page='information')
+
+@app.route("/dermatitis")
+def dermatitis():
+    return render_template('dermatitis.html', active_page='information')
+
+@app.route("/demodicosis")
+def demodicosis():
+    return render_template('demodicosis.html', active_page='information')
+
+@app.route("/fungal_infection")
+def fungal_infection():
+    return render_template('fungal_infection.html', active_page='information')
+
 if __name__ == "__main__":
     app.run(debug=True)
