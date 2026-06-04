update the image name overflow in the detection page when uploaded  by user, fix the fungal infection disease name broken to next line, fix the css for the skin diseases modules- gap between bottom information card module with footer

app.py
@@ -37,7 +37,7 @@ model = tf.keras.models.load_model(MODEL_PATH)
 #Matching the class labels with the one trained in the google colab
 CLASS_NAMES = [
     'Dermatitis',
-    'Fungal_infections',
+    'Fungal Infections',
     'Healthy',
     'Hypersensitivity',
     'Demodicosis',